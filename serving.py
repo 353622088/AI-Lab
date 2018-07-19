1cd1e4cbd7e49129c07654c6a3f673876ae56b28
Dxq <chk0125@126.com>
2018-07-19 15:48:51 +0800

    init

--- serving.py
+++ serving.py
@@ -29,29 +29,29 @@ trx = np.zeros(shape=[1, 224, 224, 3])
 request.inputs['features'].CopyFrom(tf.contrib.util.make_tensor_proto(trx, shape=[1, 224, 224, 3]))
 print(request)
 result = stub.Predict(request, FLAGS.request_timeout)
-
-def main():
-    # Generate inference data
-    keys = numpy.asarray([1, 2, 3, 4])
-    keys_tensor_proto = tf.contrib.util.make_tensor_proto(keys, dtype=tf.int32)
-    features = numpy.asarray(
-        [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1, 1, 1, 1, 1, 1, 1, 1],
-         [9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 9, 9, 9, 9, 9, 9, 9, 9]])
-    features_tensor_proto = tf.contrib.util.make_tensor_proto(
-        features, dtype=tf.float32)
-
-    # request.model_spec.name = FLAGS.model_name
-    # if FLAGS.model_version > 0:
-    #     request.model_spec.version.value = FLAGS.model_version
-    # if FLAGS.signature_name != "":
-    #     request.model_spec.signature_name = FLAGS.signature_name
-    # request.inputs["keys"].CopyFrom(keys_tensor_proto)
-    # request.inputs["features"].CopyFrom(features_tensor_proto)
-    #
-    # Send request
-    # result = stub.Predict(request, FLAGS.request_timeout)
-    # print(result)
-
-
-if __name__ == "__main__":
-    main()
+print(result)
+# def main():
+#     # Generate inference data
+#     keys = numpy.asarray([1, 2, 3, 4])
+#     keys_tensor_proto = tf.contrib.util.make_tensor_proto(keys, dtype=tf.int32)
+#     features = numpy.asarray(
+#         [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1, 1, 1, 1, 1, 1, 1, 1],
+#          [9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 9, 9, 9, 9, 9, 9, 9, 9]])
+#     features_tensor_proto = tf.contrib.util.make_tensor_proto(
+#         features, dtype=tf.float32)
+#
+#     # request.model_spec.name = FLAGS.model_name
+#     # if FLAGS.model_version > 0:
+#     #     request.model_spec.version.value = FLAGS.model_version
+#     # if FLAGS.signature_name != "":
+#     #     request.model_spec.signature_name = FLAGS.signature_name
+#     # request.inputs["keys"].CopyFrom(keys_tensor_proto)
+#     # request.inputs["features"].CopyFrom(features_tensor_proto)
+#     #
+#     # Send request
+#     # result = stub.Predict(request, FLAGS.request_timeout)
+#     # print(result)
+#
+#
+# if __name__ == "__main__":
+#     main()
